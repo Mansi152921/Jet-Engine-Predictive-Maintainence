befb0157d2c33dd109aaba1edf191bfbb95eb1d0
Mansi <mansi@email.com>
2025-10-22 18:25:51 +0530

    Changed Train_model.py script for databricks volume

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -34,6 +34,9 @@ def train_multiple_models(final_datapath, uc_model_name):
     
     logger.info('Starting model training and selection process...')
 
+    experiment_name = f"{uc_model_name}_experiment"
+    mlflow.set_experiment(experiment_name=experiment_name)
+    
     # --- Load Final Data from UC Volume ---
     # The path is now a direct Volume path, not a relative OS path
     X_train_path = os.path.join(final_datapath, 'X_train.csv')
